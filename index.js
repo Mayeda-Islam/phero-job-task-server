8276d16d4bad08d15b4793e4a6521c299bef6bac
unknown <mayedakonika@gmail.com>
2023-02-18 19:12:39 +0600

    about api and updated api added

--- index.js
+++ index.js
@@ -17,12 +17,42 @@ const client = new MongoClient(uri, {
 async function run() {
   const database = client.db("phero-job-task");
   const socialPostCollection = database.collection("social-post");
+  const commentCollection = database.collection("comment");
+  const aboutCollection = database.collection("about");
   try {
     app.post("/social-status", async (req, res) => {
       const body = req.body;
       const date = new Date().toLocaleTimeString("en-US");
       const userPost = { ...body, date };
       const result = await socialPostCollection.insertOne(userPost);
+      res.send(result);
+    });
+    app.post("/social-status/comment", async (req, res) => {
+      const commentBody = req.body;
+      const result = await commentCollection.insertOne(commentBody);
+      res.send(result);
+    });
+    app.get("/comments/:postId", async (req, res) => {
+      const postId = req.params.postId;
+      console.log("post id".postId);
+      const filter = { postId };
+      const result = await commentCollection.find(filter).toArray();
+      res.send(result);
+    });
+    app.get("/about", async (req, res) => {
+      const query = {};
+      const result = await aboutCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.patch("/about/:id", async (req, res) => {
+      const aboutId = req.params.id;
+
+      const updateInfo = req.body;
+      const query = { _id: new ObjectId(aboutId) };
+      const updateDoc = {
+        $set: updateInfo,
+      };
+      const result = await aboutCollection.updateOne(query, updateDoc);
       res.send(result);
     });
     app.patch("/social-status/:id", async (req, res) => {
